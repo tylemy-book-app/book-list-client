06eb4c3c091dab7df14d6be0db5fe618b2b059ea
Jeremy Crawford <jfcraw08@gmail.com>
2018-03-01 13:10:31 -0800

    touched and pre-populated admin js file

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -4,4 +4,7 @@ page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/add', app.bookView.initAddForm);
 page('/books/:book_id', ctx => app.Book.fetchOne(ctx, app.bookView.initDetailPage));
 
+
+page('/admin', (ctx, next) => app.adminView.initAdminPage(ctx, next), (ctx) => app.adminView.verify);
+
 page();
